3abf7e7ebd7353116f317cd317b310c17b4ab927
Harsh Balolia <harshbalolia2006@gmail.com>
2025-11-01 01:50:50 +0530

    Refactor Vehicle class

--- src/models/Vehicle.java
+++ src/models/Vehicle.java
@@ -1,18 +1,14 @@
 package models;
 
-// This is an "abstract" class. It's a blueprint for other classes.
-// You cannot create an object directly from an abstract class.
 public abstract class Vehicle {
-    // 'protected' means these fields can be accessed by this class and its children.
-    protected String vehicleId;    // e.g., "12401" for a train, "UK07-1234" for a bus, "6E-2045" for a flight
-    protected String vehicleType;  // "Train", "Bus", or "Flight"
+    protected String vehicleId;
+    protected String vehicleType;
     protected String source;
     protected String destination;
     protected String departureTime;
     protected int totalSeats;
     protected int availableSeats;
 
-    // This is the constructor. It's used to initialize the object.
     public Vehicle(String vehicleId, String vehicleType, String source, String destination, String departureTime, int totalSeats) {
         this.vehicleId = vehicleId;
         this.vehicleType = vehicleType;
@@ -20,10 +16,9 @@ public abstract class Vehicle {
         this.destination = destination;
         this.departureTime = departureTime;
         this.totalSeats = totalSeats;
-        this.availableSeats = totalSeats; // When a vehicle is created, all seats are available.
+        this.availableSeats = totalSeats; 
     }
 
-    // --- These are called "Getters". They allow us to read the values of the fields. ---
     public String getVehicleId() { return vehicleId; }
     public String getVehicleType() { return vehicleType; }
     public String getSource() { return source; }
@@ -32,12 +27,10 @@ public abstract class Vehicle {
     public int getTotalSeats() { return totalSeats; }
     public int getAvailableSeats() { return availableSeats; }
 
-    // --- This is a "Setter". It allows us to change the value of a field. ---
     public void setAvailableSeats(int availableSeats) {
         this.availableSeats = availableSeats;
     }
 
-    // A helpful method for displaying vehicle info in a user-friendly way.
     @Override
     public String toString() {
         return "Type: " + vehicleType +
@@ -47,7 +40,6 @@ public abstract class Vehicle {
                " | Seats Available: " + availableSeats;
     }
 
-    // A method to convert vehicle data into a comma-separated string for file saving.
     public String toCsvString() {
         return vehicleId + "," + vehicleType + "," + source + "," + destination + "," + departureTime + "," + totalSeats;
     }
